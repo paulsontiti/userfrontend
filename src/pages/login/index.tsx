a8d06dafe9e5ca7904620dfe813f3160e162be14
Paulson Nwoha <paulsontiti@gmail.com>
2022-12-21 10:34:12 +0100

    add mobileTop to login and signup pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,6 +7,7 @@ import { login, UserState } from '../../AppStore/slices/UserSlice';
 import { useAppDispatch, useAppSelector } from '../../AppStore/store';
 import { LoginDetails } from '../../types';
 import { token } from '../../theme';
+import MobileTopBar from '../global/mobiletopbar';
 
 const initialValues: LoginDetails = {
   email: '',
@@ -49,75 +50,79 @@ const Login = () => {
   }, [user, navigate, redirect]);
 
   return (
-    <Box sx={{ m: '5rem 1rem' }}>
-      <Box sx={{ mb: '1rem' }}>
-        <Typography component='h5'> Enter Your Login Details </Typography>
-        <Typography component='span'> don't have an account? </Typography>
-        <Link
-          to={`/signup?redirect=${redirect}`}
-          style={{
-            textDecoration: 'none',
-            color: colors.greenAscent[500],
-          }}
+    <>
+      <MobileTopBar />
+
+      <Box sx={{ m: '5rem 1rem' }}>
+        <Box sx={{ mb: '1rem' }}>
+          <Typography component='h5'> Enter Your Login Details </Typography>
+          <Typography component='span'> don't have an account? </Typography>
+          <Link
+            to={`/signup?redirect=${redirect}`}
+            style={{
+              textDecoration: 'none',
+              color: colors.greenAscent[500],
+            }}
+          >
+            Sign Up
+          </Link>
+        </Box>
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={userSchema}
         >
-          Sign Up
-        </Link>
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                width='100%'
+                gap='2rem'
+                gridTemplateColumns='repeat(4,minmax(0,1fr))'
+              >
+                <TextField
+                  fullWidth
+                  variant='filled'
+                  type='text'
+                  label='Email'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name='email'
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                />
+                <TextField
+                  fullWidth
+                  variant='filled'
+                  type='password'
+                  label='Password'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.password}
+                  name='password'
+                  error={!!touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                />
+                <Button type='submit' color='secondary' variant='contained'>
+                  Login
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
       </Box>
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={userSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display='flex'
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-              width='100%'
-              gap='2rem'
-              gridTemplateColumns='repeat(4,minmax(0,1fr))'
-            >
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='Email'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name='email'
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='password'
-                label='Password'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                name='password'
-                error={!!touched.password && !!errors.password}
-                helperText={touched.password && errors.password}
-              />
-              <Button type='submit' color='secondary' variant='contained'>
-                Login
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
-    </Box>
+    </>
   );
 };
 export default Login;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -6,7 +6,8 @@ import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { token } from '../../theme';
 import { signup, UserState } from '../../AppStore/slices/UserSlice';
 import { useAppDispatch, useAppSelector } from '../../AppStore/store';
-import { signUpDetails } from '../../types';
+import { IUser, signUpDetails } from '../../types';
+import MobileTopBar from '../global/mobiletopbar';
 
 const initialValues: signUpDetails = {
   firstName: '',
@@ -43,7 +44,9 @@ const SignUp = () => {
   const dispatch = useAppDispatch();
 
   //get user from state
-  const user = useAppSelector((state: { user: UserState }) => state.user.user);
+  const user: IUser = useAppSelector(
+    (state: { user: UserState }) => state.user.user
+  );
 
   //login handler
   const handleFormSubmit = (values: signUpDetails) => {
@@ -51,131 +54,82 @@ const SignUp = () => {
     if (user) navigate(redirect);
   };
   return (
-    <Box sx={{ m: '1rem 1rem' }}>
-      <Box sx={{ mb: '1rem' }}>
-        <Typography component='h5'> Create An Account </Typography>
-        <Typography component='span'> have an account? </Typography>
-        <Link
-          to={`/login?redirect=${redirect}`}
-          style={{
-            textDecoration: 'none',
-            color: colors.greenAscent[500],
-          }}
-        >
-          Login
-        </Link>
-      </Box>
+    <>
+      <MobileTopBar />
+      <Box sx={{ m: '1rem 1rem' }}>
+        <Box sx={{ mb: '1rem' }}>
+          <Typography component='h5'> Create An Account </Typography>
+          <Typography component='span'> have an account? </Typography>
+          <Link
+            to={`/login?redirect=${redirect}`}
+            style={{
+              textDecoration: 'none',
+              color: colors.greenAscent[500],
+            }}
+          >
+            Login
+          </Link>
+        </Box>
 
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={userSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display='grid'
-              gap='2rem'
-              gridTemplateColumns='repeat(4,minmax(0,1fr))'
-              sx={{
-                '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='First Name'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.firstName}
-                name='firstName'
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
-                sx={{ gridColumn: 'span 2' }}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='Last Name'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.lastName}
-                name='lastName'
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: 'span 2' }}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='Email'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name='email'
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                sx={{ gridColumn: 'span 4' }}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='Contact'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.contact}
-                name='contact'
-                error={!!touched.contact && !!errors.contact}
-                helperText={touched.contact && errors.contact}
-                sx={{ gridColumn: 'span 4' }}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='password'
-                label='Password'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-                name='password'
-                error={!!touched.password && !!errors.password}
-                helperText={touched.password && errors.password}
-                sx={{ gridColumn: 'span 4' }}
-              />
-              <TextField
-                fullWidth
-                variant='filled'
-                type='text'
-                label='Username'
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.userName}
-                name='userName'
-                error={!!touched.userName && !!errors.userName}
-                helperText={touched.userName && errors.userName}
-                sx={{ gridColumn: 'span 4' }}
-              />
-            </Box>
-            <Box display='flex' justifyContent='end' mt='1rem'>
-              <Button type='submit' color='secondary' variant='contained'>
-                Create Account
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
-    </Box>
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={userSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box
+                display='grid'
+                gap='2rem'
+                gridTemplateColumns='repeat(4,minmax(0,1fr))'
+                sx={{
+                  '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant='filled'
+                  type='text'
+                  label='Email'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name='email'
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+                <TextField
+                  fullWidth
+                  variant='filled'
+                  type='password'
+                  label='Password'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.password}
+                  name='password'
+                  error={!!touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+              </Box>
+              <Box display='flex' justifyContent='end' mt='1rem'>
+                <Button type='submit' color='secondary' variant='contained'>
+                  Create Account
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </>
   );
 };
 export default SignUp;
